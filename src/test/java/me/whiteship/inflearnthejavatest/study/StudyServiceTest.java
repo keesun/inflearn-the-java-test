bf51d7d89e35f7310fceba912cdfa0f8997d0b82
Keesun Baik <whiteship2000@gmail.com>
2019-11-28 16:16:10 -0800

    20. Stubbing answers

--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
@@ -23,9 +23,11 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
+    @Mock MemberService memberService;
+    @Mock StudyRepository studyRepository;
+
     @Test
-    void createNewStudy(@Mock MemberService memberService,
-                            @Mock StudyRepository studyRepository) {
+    void createNewStudy() {
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
 
@@ -35,12 +37,10 @@ class StudyServiceTest {
 
         Study study = new Study(10, "테스트");
 
-        // TODO memberService 객체에 findById 메소드를 1L 값으로 호출하면 member 객체를 리턴하도록 Stubbing
-        // TODO studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        when(studyRepository.save(study)).thenReturn(study);
 
         studyService.createNewStudy(1L, study);
-
-        assertNotNull(study.getOwner());
         assertEquals(member, study.getOwner());
     }
 
